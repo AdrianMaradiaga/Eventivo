c65c2e3c0ddbab35c9b379d89b3ef0a990cb3fbf
_adrianmaradiaga <adrianmaradiaga07@gmail.com>
2023-07-09 14:09:40 -0600

    Validaciones de atributos vacios

--- app/src/main/java/hn/uth/eventivo/ui/creacionEventos/CreacionEventoFragment.java
+++ app/src/main/java/hn/uth/eventivo/ui/creacionEventos/CreacionEventoFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,19 +18,41 @@ public class CreacionEventoFragment extends Fragment {
     private FragmentCreacionEventoBinding binding;
     private CreacionEventoViewModel viewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-    binding = FragmentCreacionEventoBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentCreacionEventoBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         viewModel = new ViewModelProvider(this).get(CreacionEventoViewModel.class);
 
         binding.btnGuardar.setOnClickListener(v -> {
-        Eventos nuevo = new Eventos(binding.tilTema.getText().toString(), binding.tilExpositor.getText().toString(),
-                binding.dtFecha.getText().toString(),
-                binding.bxEstado.getText().toString(),
-                binding.tilDetalle.getText().toString());;
+            String tema = binding.tilTema.getText().toString().trim();
+            String expositor = binding.tilExpositor.getText().toString().trim();
+            String fecha = binding.dtFecha.getText().toString().trim();
+            String detalle = binding.tilDetalle.getText().toString().trim();
 
+            if (tema.isEmpty()) {
+                Toast.makeText(getContext(), "Ingrese el tema", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (expositor.isEmpty()) {
+                Toast.makeText(getContext(), "Ingrese el expositor", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (fecha.isEmpty()) {
+                Toast.makeText(getContext(), "Ingrese la fecha", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (detalle.isEmpty()) {
+                Toast.makeText(getContext(), "Ingrese el detalle", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+            // Realiza la acción de guardar el evento
+            Eventos nuevo = new Eventos(tema, expositor, fecha, "", detalle);
             viewModel.insert(nuevo);
 
             binding.tilTema.getText().clear();
@@ -38,10 +62,11 @@ public class CreacionEventoFragment extends Fragment {
             binding.tilDetalle.getText().clear();
 
             Navigation.findNavController(v).navigateUp();
-
         });
+
         return root;
     }
+
 
 @Override
     public void onDestroyView() {
